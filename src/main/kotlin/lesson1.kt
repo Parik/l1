bc41f84ad4934279f4cf7d61ad75967660f1aa15
Igor Ponomarev <56533018+Parik@users.noreply.github.com>
2021-05-06 15:10:17 +0300

    Initial commit

---
+++ src/main/kotlin/lesson1.kt
@@ -0,0 +1,67 @@
+import kotlin.Int as Int1
+import kotlin.text.toInt as toInt
+
+val firstName : String = "Masha"
+var lastName : String = "Ivanova"
+var age = 26
+const val DEBUG = true
+
+//===============MAIN===================//
+fun main(args : Array<String>)
+{
+//    lastName = "Kukaracha"
+//    println(firstName)
+//    println(lastName)
+//    println(age)
+
+//    val temperature = 26
+//    val condition = if (temperature < -5 /*|| age < 28*/) "Холодно"
+//    else if (temperature < 5)
+//        "Около нуля"
+//    else
+//        "Жарко"
+//    println(condition)
+
+//    val temperature = 55
+//    val condition = when {
+//        temperature < 30 -> "Terpimo"
+//        temperature > 50 -> "Norm"
+//        else -> "innaya hren"
+//    }
+//    println(condition)
+
+//nullable
+//    println("Введите possition")
+//    var position : String? = readLine()
+//    position = null
+//    if (position != null)
+//    println("length is " + position.length)
+//    println("length is " + position?.length)
+
+//    print("Введите число: ")
+//    var input = readLine()?.toInt()
+//    println("Введенноечисло $input")
+
+//val obg : Any = "15"
+//    if (obg is String){
+//        println ("String is len: ${obg.length}")
+//    }
+
+////Ask for user name
+//    print("Enter Name: ")
+//    val name    = readLine()!!.toString()
+//    //Ask For Age
+//    print("Enter Age: ")
+//    val age  = readLine()!!.toInt()
+//    print("Enter GPA: ")
+//    val GPA  =  readLine()!!.toDouble()
+//
+//
+//    println("===== User info ===== ")
+//    println("Your Name is : $name")
+//    println("Your Age: $age"   )
+//    println("Your GPA: $GPA" )
+
+
+}
+
